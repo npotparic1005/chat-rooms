067725dc60a5b85d86a1c2b482f8d5d689973a35
Nikolina Potparic <npotparic1005@gmail.com>
2024-01-13 12:05:09 -0800

    Popravi reply message

--- src/rs/raf/pds/v4/z5/messages/ReplyMessageRequest.java
+++ src/rs/raf/pds/v4/z5/messages/ReplyMessageRequest.java
@@ -1,5 +1,17 @@
 package rs.raf.pds.v4.z5.messages;
 
 public class ReplyMessageRequest {
+    private String replyMessage;
+    private String originalMessage;
 
+    public ReplyMessageRequest(String replyMessage, String originalMessage) {
+        this.replyMessage = "Replying to: " + originalMessage + ": " + replyMessage;
+        this.originalMessage = originalMessage;
+    }
+
+    public ReplyMessageRequest() {}
+
+    public String getReplyMessage() {
+        return replyMessage;
+    }
 }
